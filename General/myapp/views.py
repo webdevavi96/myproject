4d61617ef40763c789f0e764b9d83c9b63101669
webdevavi96 <avinashchaurasiya902@gmail.com>
2024-10-13 14:36:10 +0530

    update views.py

--- General/myapp/views.py
+++ General/myapp/views.py
@@ -2,30 +2,30 @@ from django.shortcuts import render
    	 
    	 # Create your views here.
    	 
-   	 def index(request):
-   	     return render(request, 'index.html')
+def index(request):
+    return render(request, 'index.html')
    	 
-   	 def home(request):
-   	     return render(request, 'home.html')
+def home(request):
+    return render(request, 'home.html')
    	 
-   	 def about(request):
-   	     return render(request, 'about.html')
+def about(request):
+    return render(request, 'about.html')
    	 
-   	 def ourteam(request):
-   	     return render(request, 'ourteam.html')
+def ourteam(request):
+    return render(request, 'ourteam.html')
    	 
-   	 def gallery(request):
-   	     return render(request, 'gallery.html')
+def gallery(request):
+    return render(request, 'gallery.html')
    	 
-   	 def notes(request):
-   	     return render(request, 'notes.html')
+def notes(request):
+    return render(request, 'notes.html')
    	 
-   	 def registration(request):
-   	     return render(request, 'registration.html')
+def registration(request):
+    return render(request, 'registration.html')
+    
+def contactus(request):
+   	return render(request, 'contactus.html')
    	 
-   	 def contactus(request):
-   	     return render(request, 'contactus.html')
-   	 
-   	 def joinus(request):
-   	     return render(request, 'joinus.html')
+def joinus(request):
+    return render(request, 'joinus.html')
    	 
