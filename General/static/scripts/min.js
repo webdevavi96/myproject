a93018332f32a76021f97fb2d909a497791db00b
webdevavi96 <avinashchaurasiya902@gmail.com>
2024-10-21 18:05:01 +0530

    update min.js

--- General/static/scripts/min.js
+++ General/static/scripts/min.js
@@ -1,1 +1,9 @@
-// alert("It's Working");
+ alert("It's Working");
+ 
+ function next() {
+   const next = document.getElementById('next')
+   const target = document.getElementById('landing')
+   target.style.display = 'none'
+ }
+ 
+ document.addEventListener(onclick, next())
