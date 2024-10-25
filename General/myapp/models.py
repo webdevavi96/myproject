5a0200208537fc4f12c0b110d9f81e5c52cc34dc
webdevavi96 <avinashchaurasiya902@gmail.com>
2024-10-25 16:35:54 +0530

    update models.py

--- General/myapp/models.py
+++ General/myapp/models.py
@@ -1,13 +1,11 @@
-from django.db import models
-
-# Create your models here.
 
 from django.db import models
+#Create your models here
 
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)  # Store hashed password
+    password = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
