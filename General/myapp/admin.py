4cd86608ef64a012a2e16eaa11189d43a9c4c97a
webdevavi96 <avinashchaurasiya902@gmail.com>
2024-10-25 17:08:08 +0530

    update

--- General/myapp/admin.py
+++ General/myapp/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import CustomUser  # Update the import statement
+from .models import User  # Update the import statement
 
-admin.site.register(CustomUser)  # Register the renamed model
+admin.site.register(User)  # Register the renamed model
--- General/myapp/models.py
+++ General/myapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class CustomUser(models.Model):  # Changed from User to CustomUser
+class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
