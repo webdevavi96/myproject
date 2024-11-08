81f245e15d0b379973068cd83af51b0070632169
webdevavi96 <avinashchaurasiya902@gmail.com>
2024-11-08 07:26:40 +0000

    adding user authientication

--- General/myapp/views.py
+++ General/myapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-   	 
-   	 # Create your views here.
-   	 
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout 	 
+from django.contrib.auth.decorators import login_required   	
+     # Create your views here.
+@login_required(login_url='index')	 
 def index(request):
     return render(request, 'index.html')
    	 
@@ -22,7 +24,26 @@ def contactus(request):
    	return render(request, 'contactus.html')
    	 
 def register(request):
-    return render(request, 'register.html')
-   	 
+    if request.method == 'POST':
+        userName = request.POST.get('userName')
+        userEmail = request.POST.get('userEmail')
+        userPassword = request.POST.get('userPassword')
+        confirmPassword = request.POST.get('confirmPassword')
+        if userPassword != confirmPassword:
+           return HttpResponse("Password did not macth. Please enter a valid Password.")
+        else:
+            newUser = User.objects.createUser(userName,userEmail,userPassword)
+            newUser.save()
+            return redirect('login.html')
+    return render('register.html')
 def login(request):
+    if request.method == 'POST':
+       userName = request.POST.get('userName')
+       userPassword = request.POST.get('userPassword')
+       user = authenticate(request, userName = userName, userPassword = userPassword)
+       if user is not None:
+           login(request, user)
+           return redirect('home')
+       else:
+           return HttpResponse("username or password is Incorrect. Please check and enter agqin to Log in into your account.")
     return render(request, 'login.html')
