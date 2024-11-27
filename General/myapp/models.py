43ea8d622c796b13bf6aff9cea78afa234ebb023
webdevavi96 <avinaschaurasiya902@gmail.com>
2024-11-27 20:11:59 +0530

    update models.py

--- General/myapp/models.py
+++ General/myapp/models.py
@@ -6,4 +6,4 @@ class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
 
  def __str__(self):
-        return self.username
+    return self.username
