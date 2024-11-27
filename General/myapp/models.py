d7ff4d9dd0aad8fc78593a10851b08a4cc40c789
webdevavi96 <avinaschaurasiya902@gmail.com>
2024-11-27 20:24:24 +0530

    update models.py

--- General/myapp/models.py
+++ General/myapp/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-        from django.db import models
+from django.db import models
         
-        class CustomUser(AbstractUser):
-            phone_number = models.CharField(max_length=15, blank=True, null=True)
+class CustomUser(AbstractUser):
+   phone_number = models.CharField(max_length=15, blank=True, null=True)
