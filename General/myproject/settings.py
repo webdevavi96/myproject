43ce4233e66b07c50337bba99afde66067d70f1e
webdevavi96 <avinaschaurasiya902@gmail.com>
2024-12-11 20:34:50 +0530

    update settings.py

--- General/myproject/settings.py
+++ General/myproject/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -127,6 +127,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
 	os.path.join(BASE_DIR,'static')
 	]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
