933678df518d4449552225bfc44a57b90de491b0
webdevavi96 <avinaschaurasiya902@gmail.com>
2024-12-11 19:56:02 +0530

    update settings.py

--- General/myproject/settings.py
+++ General/myproject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'material',
-    'material.admin.default',
+    'material.admin',
     'widget_tweaks',
     'myapp',
 ]
